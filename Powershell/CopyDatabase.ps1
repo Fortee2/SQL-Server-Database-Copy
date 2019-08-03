06c764ff1db154f17c6c723466bb54301e8c2be5
Randy Costner <costner.randy@gmail.com>
2019-08-03 10:52:49 -0400

    Renames output log files

--- Powershell/CopyDatabase.ps1
+++ Powershell/CopyDatabase.ps1
@@ -27,12 +27,12 @@ foreach($sql in $DS.Tables[0].Rows){
         Invoke-Expression $bcp
         Write-Output $bcp
 
-        $bcp = "bcp $($sql.SchemaName).$($sql.TableName) out $($tempPath)$($sql.TableName).bcp -S $($ServerName) -d $($SourceDatabase) -U $($UserName) -P $($Password) -E -n -t -e $($logPath)$($sql.TableName)_in_err.txt"
+        $bcp = "bcp $($sql.SchemaName).$($sql.TableName) out $($tempPath)$($sql.TableName).bcp -S $($ServerName) -d $($SourceDatabase) -U $($UserName) -P $($Password) -E -n -t -e $($logPath)$($sql.TableName)_out_err.txt"
         Invoke-Expression $bcp
         Write-Output $bcp
 
         #add code to import in to use format file
-        $bcp = "bcp $($sql.SchemaName).$($sql.TableName) in $($tempPath)$($sql.TableName).bcp -S $($DestinationServer) -d $($DestinationDB) -T -t -E -f $($tempPath)$($sql.TableName).fmt -e $($logPath)$($sql.TableName)_out_err.txt"
+        $bcp = "bcp $($sql.SchemaName).$($sql.TableName) in $($tempPath)$($sql.TableName).bcp -S $($DestinationServer) -d $($DestinationDB) -T -t -E -f $($tempPath)$($sql.TableName).fmt -e $($logPath)$($sql.TableName)_in_err.txt"
         Invoke-Expression $bcp
         Write-Output $bcp
 
